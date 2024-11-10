72bcb21186fc0419d278e2215dc8d45121057ebc
Avashneupane9857 <juisch20220011252@gmail.com>
2024-11-10 11:19:02 +0545

    :bug: auth Err solved

--- 2d-Metaverse/apps/httpServer/src/controllers/SignUp.ts
+++ 2d-Metaverse/apps/httpServer/src/controllers/SignUp.ts
@@ -1,33 +1,45 @@
-
 import { SignUpSchema } from "../types"
-import {prisma} from "@repo/db/prisma"
+import { prisma } from "@repo/db/prisma"
 import bcrypt from "bcrypt"
-export const SignUp=async(req:any,res:any)=>{
-const parsedData=SignUpSchema.safeParse(req.body)
-console.log(parsedData)
-if(!parsedData.success){
-    return  res.status(400).json({msg:"Validataion err"})
-    return
-}
-try
-{
-const hashedPassword=await bcrypt.hash(parsedData.data.password,10)
-const createUser=await prisma.user.create({
-    data:{
-        username:parsedData.data.username,
-        password:hashedPassword,
-        role:parsedData.data.type
-    }
-})
-res.status(200).json({userId:createUser.id})
-return
-}
-catch(e){
 
-    console.log("error in signup",e)
-    return res.status(403).json({
-msg:"Erro in signup"
+export const SignUp = async (req: any, res: any) => {
+  const parsedData = SignUpSchema.safeParse(req.body)
+  
+  if (!parsedData.success) {
+    return res.status(400).json({ msg: "Validation error" })
+  }
+
+  try {
+   
+    const check = await prisma.user.findUnique({
+      where: {
+        username: parsedData.data.username,
+      },
     })
-}
 
+    if (check) {
+  
+      return res.status(400).json({ msg: "User already signed up" })
+    }
+
+
+    const hashedPassword = await bcrypt.hash(parsedData.data.password, 10)
+
+
+    const createUser = await prisma.user.create({
+      data: {
+        username: parsedData.data.username,
+        password: hashedPassword,
+        role: parsedData.data.type,
+      },
+    })
+
+    res.status(200).json({ userId: createUser.id })
+    return
+  } catch (e) {
+    console.log("Error in signup", e)
+    return res.status(500).json({
+      msg: "Error in signup",
+    })
+  }
 }

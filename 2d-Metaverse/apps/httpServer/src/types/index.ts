aea7817f3b6d13ebbbe9b2998813a3ba94de69bc
Avashneupane9857 <juisch20220011252@gmail.com>
2024-11-10 23:30:12 +0545

    http Server finished

--- 2d-Metaverse/apps/httpServer/src/types/index.ts
+++ 2d-Metaverse/apps/httpServer/src/types/index.ts
@@ -35,3 +35,29 @@ export const AddElementSchema = z.object({
   x: z.number(),
   y: z.number(),
 })
+
+export const CreateElementSchema=z.object({
+  imageUrl:z.string(),
+  width:z.number(),
+  height:z.number(),
+  static:z.boolean()
+})
+export const UpdateElementSchema = z.object({
+  imageUrl: z.string(),
+})
+
+export const CreateAvatarSchema = z.object({
+  name: z.string(),
+  imageUrl: z.string(),
+})
+
+export const CreateMapSchema = z.object({
+  thumbnail: z.string(),
+  dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
+  name: z.string(),
+  defaultElements: z.array(z.object({
+      elementId: z.string(),
+      x: z.number(),  
+      y: z.number(),
+  }))
+})
